786d0927be2fcec4ce93e8c019c3e157793ceb28
imsarann <shitisaesthetic@gmail.com>
2025-02-14 00:15:49 +0530

    recreated frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Chat from './components/Chat'
+
+function App() {
+
+  return (
+    <div className='bg-black  min-h-screen flex justify-center items-center'>
+      <Chat/>
+
+    </div>
+  )
+}
+
+export default App
